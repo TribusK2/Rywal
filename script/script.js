26c42bc15c44127936537a41a32a566e55ea8ea6
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2019-04-18 23:19:09 +0200

    jquery added

--- script/script.js
+++ script/script.js
@@ -5,18 +5,22 @@ var currentScrollPos = window.pageYOffset;
 
     // menu scroll effect
   if (currentScrollPos <= 20) {
-    document.getElementById("brandLogo").style.height = "90px";
+    $("#brandLogo").css("height", "90px")
+    // document.getElementById("brandLogo").style.height = "90px";
   } else {
-    document.getElementById("brandLogo").style.height = "50px";
+    $("#brandLogo").css("height", "50px")
+    // document.getElementById("brandLogo").style.height = "50px";
   }
   
     // menu scroll effect end
 
     // road scroll effect
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("roadPic").src = "img/asphalt-road.jpg";
+    $("#roadPic").attr("src", "img/asphalt-road.jpg")
+    // document.getElementById("roadPic").src = "img/asphalt-road.jpg";
   } else {
-    document.getElementById("roadPic").src = "img/forest-road.jpg";
+    $("#roadPic").attr("src", "img/forest-road.jpg")
+    // document.getElementById("roadPic").src = "img/forest-road.jpg";
   }
   prevScrollpos = currentScrollPos;
     // road scroll effect end
