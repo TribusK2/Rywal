e3b2678a0ef2b9462fb3cd9c75dfb4fed8f50d98
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2019-05-26 07:20:48 +0200

    Bug fixed - confirm.js

--- script/confirm.js
+++ script/confirm.js
@@ -13,18 +13,19 @@ $(document).ready(function(){
 
         // verification code
         var code = 1 + Math.floor(Math.random() * 1000000);
-        // console.log(code);
+        console.log(code);
         // verification code end
 
         // back button
         var backButton = $('#backButton');
         backButton.click(function(){
-        window.location.href = "career.html";
+        window.location.href = "career.php";
         });
         // back button end
 
         // Button submit
-        var textEx = /^[a-zA-Z]+$/;
+        var nameEx = /^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ ]+$/;
+        var sNameEx = /^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ -]+$/;
         var emaiEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
         $('#confirmForm').submit(function(){
@@ -34,27 +35,27 @@ $(document).ready(function(){
             var checkCheck = $('#checkConfirmed').prop('checked');
             var codeCheck = $('#codeConfirmed').val();
             if (typeof(Storage) !== "undefined"){   
-                if (textEx.test(nameCheck) && textEx.test(sNameCheck) && emaiEx.test(emailCheck) && checkCheck == true && codeCheck == code){
+                if (nameEx.test(nameCheck) && sNameEx.test(sNameCheck) && emaiEx.test(emailCheck) && checkCheck == true && codeCheck == code){
                         sessionStorage.removeItem("name");
                         sessionStorage.removeItem("sName");
                         sessionStorage.removeItem("email");
                         sessionStorage.removeItem("check");
                 } else {
-                    if(!textEx.test(nameCheck)){
+                    if(!nameEx.test(nameCheck)){
                         $('#input1Confirmed').nextAll('small').removeClass('validationAlert'); 
-                    }
-                    if(!textEx.test(sNameCheck)){
+                    };
+                    if(!sNameEx.test(sNameCheck)){
                         $('#input2Confirmed').nextAll('small').removeClass('validationAlert'); 
-                    }
+                    };
                     if(!emaiEx.test(emailCheck)){
                         $('#emailConfirmed').nextAll('small').removeClass('validationAlert'); 
-                    }
+                    };
                     if(checkCheck == false){
                         $('#checkConfirmed').nextAll('small').removeClass('validationAlert'); 
-                    }
-                    if(codeCheck !== code){
+                    };
+                    if(codeCheck != code){
                         $('#codeConfirmed').nextAll('small').removeClass('validationAlert'); 
-                    }
+                    };
                     return false;
                 };    
             } else {
